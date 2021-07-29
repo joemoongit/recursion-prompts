57780e4bd011ef1f3a2daf1f38e798e961b61c87
joemoongit <joemoondev@gmail.com>
2021-07-28 23:44:28 -0700

    Solved compress recursively

--- src/recursion.js
+++ src/recursion.js
@@ -416,7 +416,11 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
-};
+  if (list.length === 0) {
+    return [];
+  }
+  return ((list[0] !== list[1]) ? [list[0]] : []).concat(compress(list.slice(1, list.length)));
+}
 
 // 33. Augment every element in a list with a new value where each element is an array
 // itself.
